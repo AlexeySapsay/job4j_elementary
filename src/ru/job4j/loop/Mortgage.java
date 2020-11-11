b04978c5d6a01b5f7e578706e9ce87ef619d8097
AlekseySapsay <pheniksesalex@mail.ru>
2020-11-11 10:25:30 +0300

    5.7. Ипотека [#402089]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 0;
+
+        while (amount > 0) {
+            amount = (int) (amount + amount * percent * 0.01) - salary;
+            year += 1;
+        }
+        System.out.println(amount);
+        System.out.println(year);
+        return year;
+    }
+}
