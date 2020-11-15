c8f54e0f3872f224eefcedbf18aed0167d5bef15
AlekseySapsay <pheniksesalex@mail.ru>
2020-11-15 10:20:37 +0300

    21. indexOf с числом вхождений

---
+++ src/ru/job4j/array/IndexOfWithCount.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class IndexOfWithCount {
+    public static int indexOf(char[] string, char c, int number) {
+        int counter = 0;
+        for (int i = 0; i < string.length; i++) {
+            if ((string[i] == c) && (c == '1')) {
+                return i;
+            }
+            if (string[i] == c) {
+                counter += 1;
+                if (counter == number) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
