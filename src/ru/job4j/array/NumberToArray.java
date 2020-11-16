e9f23cc3d6f0f4d09f169fd9c187ac34a537dae4
AlekseySapsay <pheniksesalex@mail.ru>
2020-11-17 00:21:39 +0300

    41. Массив цифр

---
+++ src/ru/job4j/array/NumberToArray.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+/**
+ * Дано число int. Нужно получить массив из его цифр в обратном порядке.
+ * <p>
+ * Например
+ * <p>
+ * 12345 => {5, 4, 3, 2, 1}
+ */
+public class NumberToArray {
+    public static int[] resolve(int number) {
+        String strInt = String.valueOf(number);
+        int[] arrayInt = new int[strInt.length()];
+
+        for (int index = 0; index < strInt.length(); index++) {
+            String strBuffer = strInt.substring(index, index + 1);
+            // convert string to int
+            int intBuffer = Integer.valueOf(strBuffer);
+            //revers arrayInt
+            arrayInt[strInt.length() - index - 1] = intBuffer;
+        }
+        return arrayInt;
+    }
+}
