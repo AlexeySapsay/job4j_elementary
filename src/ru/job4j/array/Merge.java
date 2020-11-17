4b53d92e3bb536f207997a7398762e0a4705e962
AlekseySapsay <pheniksesalex@mail.ru>
2020-11-17 11:28:08 +0300

    3. Объединить два отсортированных массива3

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -3,21 +3,36 @@ package ru.job4j.array;
 /**
  * Даны два отсортированных по возрастанию массива.
  * Как без сортировки их объединить в третий массив?
- *
  */
 
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int rslIndex = 0;
-        for (int indexLeft = 0; indexLeft < left.length; indexLeft++) {
-            for (int indexRight = 0; indexRight < right.length; indexRight++) {
-                if (left[indexLeft] > right[indexRight]) {
-                    rsl[rslIndex] = right[indexRight];
-                    rslIndex += 1;
-                    System.out.println(rsl[0]);
-                }
+        int leftIndex = 0;
+        int rightIndex = 0;
 
+        if ((left.length == 0) && (right.length == 0)) return right;
+        if ((left.length == 0) && (right.length != 0)) return right;
+        if ((right.length == 0) && (right.length != 0)) return left;
+
+        for (int index = 0; index < rsl.length; index++) {
+            rsl[index] = left[leftIndex] < right[rightIndex] ? left[leftIndex++] : right[rightIndex++];
+
+            if (leftIndex == left.length) {
+                for (int j = rightIndex; j < right.length - rightIndex; j++) {
+                    rsl[index] = right[j];
+                    //j++;
+                    index++;
+                }
+                return rsl;
+            }
+            if (rightIndex == right.length) {
+                for (int k = leftIndex; k < left.length - leftIndex; k++) {
+                    rsl[index] = left[k];
+                    //k++;
+                    index++;
+                }
+                return rsl;
             }
         }
         return rsl;
