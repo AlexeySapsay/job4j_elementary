f39cbe9cc1c5a72bad142a8aa161801303d0441e
AlekseySapsay <pheniksesalex@mail.ru>
2020-11-23 22:48:41 +0300

    4. Расстояние между точками в трехмерном пространстве. [#402188]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -7,22 +7,41 @@ public class Point {
 
     private int x;
     private int y;
+    private int z;
 
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
 
+    public Point(int first, int second, int fourth) {
+        this.x = first;
+        this.y = second;
+        this.z = fourth;
+    }
+
     public double distance(Point that) {
         return Math.pow((Math.pow((this.x - that.x), 2) + Math.pow((this.y - that.y), 2)), 0.5);
+    }
+
+    public double distance3d(Point that) {
+        return Math.pow((Math.pow((this.x - that.x), 2) + Math.pow((this.y - that.y), 2) + Math.pow((this.z - that.z), 2)), 0.5);
+    }
+
+    public void info() {
+        System.out.println(String.format("Point[%s, %s, %s]", this.x, this.y, this.z));
     }
 
     public static void main(String[] args) {
         Point a = new Point(0, 0);
         Point b = new Point(2, 2);
 
-        double distance = a.distance(b);
-        System.out.println(distance);
-    }
+        Point a1 = new Point(0, 0, 10);
+        Point b1 = new Point(1, 1, 0);
 
+        double distance = a.distance(b);
+        double distance3D = a1.distance3d(b1);
+        System.out.println(distance);
+        System.out.println(distance3D);
+    }
 }
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -8,34 +8,67 @@ public class PointTest {
 
     @Test
     public void when00to20Then2() {
-        int x1 = 0;
-        int x2 = 0;
-        int y1 = 2;
-        int y2 = 0;
         double expected = 2;
-        double out = Point.distance(x1, x2, y1, y2);
+
+        Point one = new Point(0, 0);
+        Point two = new Point(2, 0);
+
+        double out = one.distance(two);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when01To22Then2() {
-        int x1 = 0;
-        int x2 = 1;
-        int y1 = 2;
-        int y2 = 2;
         double expected = 2.23;
-        double out = Point.distance(x1, x2, y1, y2);
+
+        Point one = new Point(0, 1);
+        Point two = new Point(2, 2);
+
+        double out = one.distance(two);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when11To99Then2() {
-        int x1 = 1;
-        int x2 = 1;
-        int y1 = 9;
-        int y2 = 9;
         double expected = 11.31;
-        double out = Point.distance(x1, x2, y1, y2);
+
+        Point one = new Point(1, 1);
+        Point two = new Point(9, 9);
+
+        double out = one.distance(two);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when000to000Then0() {
+        double expected = 0;
+
+        Point one = new Point(0, 0, 0);
+        Point two = new Point(0, 0, 0);
+
+        double out = one.distance3d(two);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when100to001Then0() {
+        double expected = 1.41;
+
+        Point one = new Point(1, 0, 0);
+        Point two = new Point(0, 0, 1);
+
+        double out = one.distance3d(two);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when111to999Then0() {
+        double expected = 13.856;
+
+        Point one = new Point(1, 1, 1);
+        Point two = new Point(9, 9, 9);
+
+        double out = one.distance3d(two);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
