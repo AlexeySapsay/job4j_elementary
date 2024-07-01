dac50329e63e7c414e95441d69805fcab442cb53
AlekseySapsay <pheniksesalex@mail.ru>
2024-07-01 19:18:12 +0400

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049]

--- src/ru/job4j/array/StringUsage.java
+++ src/ru/job4j/array/StringUsage.java
@@ -1,14 +1,6 @@
 package ru.job4j.array;
 
 public class StringUsage {
-    //    public static void main(String[] args) {
-//        String first = "aaabvddrr";
-//        System.out.println("Length of string FIRST: " + first.length());
-//        String second = "aaabbbvdddrrr";
-//        System.out.println("Length of string SECOND: " + second.length());
-//        String third = "abbvdddr";
-//        System.out.println("Length of string THIRD: " + third.length());
-//    }
     public static void main(String[] args) {
         String first = "aaabvddrr";
         System.out.println("Length of string FIRST: " + first.length());
